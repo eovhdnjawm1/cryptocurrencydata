a2a6282e58c6ba6fb58e1b75be6d28a92144250f
YoungJinGo <skyyj32@naver.com>
2022-07-20 23:08:02 +0900

    CoinHistory API Update

--- src/api.ts
+++ src/api.ts
@@ -15,6 +15,13 @@ export function fetchCoinTickers(coinId: string) {
 	return axios(`${BASE_URL}/tickers/${coinId}`).then(res => res.data);
 }
 
+export function fetchCoinHistory(coinId: string) {
+	const endDate = Math.floor(Date.now() / 1000);
+	const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+
+	return axios(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`).then(res => res.data)
+}
+
 // export function fetchCoinInfo(coinId: string) {
 // 	return fetch(`${BASE_URL}/coins/${coinId}`).then(response => response.json());
 // }
@@ -23,16 +30,6 @@ export function fetchCoinTickers(coinId: string) {
 // 	return fetch(`${BASE_URL}/tickers/${coinId}`).then(response => response.json());
 // }
 
-export function fetchCoinHistory(coinId: string) {
-
-	const endDate = Math.floor(Date.now() / 1000)
-	//  일주일 전
-	const startDate = endDate - 60 * 60 * 23;
-
-	return fetch(
-		`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
-	).then((response) => response.json());
-}
 
 
 export function fetchCoinPrice(coinId: string) {
