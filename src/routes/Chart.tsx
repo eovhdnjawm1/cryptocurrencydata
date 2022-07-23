7493846057d7c0db8df6182e091d51ee9bdd07af
YoungJinGo <skyyj32@naver.com>
2022-07-24 05:05:14 +0900

    coin Chart 완성

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,31 +1,111 @@
-import ApexCharts from 'apexcharts';
+import ApexCharts from 'react-apexcharts';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { fetchCoinHistory } from '../api';
+import dayjs from 'dayjs';
 
 interface ChartProps {
 	coinId: string;
 }
 
-interface IChart {
-	time_open: string;
-	time_close: string;
-	open: number;
-	high: number;
-	low: number;
-	close: number;
-	volume: number;
+interface IChartType {
+	close: string;
+	high: string;
+	low: string;
 	market_cap: number;
+	open: string;
+	time_close: number;
+	time_open: string;
+	volume: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-	console.log(coinId);
-	const { isLoading, data } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId))
+	const { isLoading, data } = useQuery<IChartType[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId))
 	console.log(data);
 
 	return (
 		<>
-			<h1>Chart</h1>
+			{isLoading ? "Loading chart..." : (
+				<ApexCharts
+					type="candlestick"
+					series={[
+						{
+							name: "Today Close Price",
+							data: data?.slice(0, 7).map((price) => {
+								return {
+									x: price.time_open,
+									y:
+										[price.open,
+										price.high,
+										price.low,
+										price.close]
+								};
+								// price.close) as number[],
+							})
+						},
+					] as any}
+					options={{
+						theme: {
+							mode: "dark",
+						},
+						chart: {
+							type: 'candlestick',
+							height: 500,
+							width: 1000,
+							toolbar: {
+								show: false,
+							},
+							zoom: {
+								enabled: false,
+							},
+							background: "transparent",
+						},
+						stroke: {
+							curve: "straight",
+							width: 2,
+							lineCap: 'butt',
+						},
+						// xaxis: {
+						// 	type: 'category',
+						// 	categories: ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+						// 	//  () => { return data?.map((price) => price.time_open)) as any }
+
+						// },
+						xaxis: {
+							type: "category",
+							labels: {
+								// datetimeFormatter: { month: "mm yy" },
+								formatter: (val) => {
+									return dayjs(val).format('MMM DD')
+								}
+							},
+						},
+						yaxis: {
+							show: true,
+							labels: {
+								formatter: (value) => `${value.toFixed(2)} $`,
+							},
+						},
+						title: {
+							text: "Today Close Price",
+							align: 'center',
+							margin: 20,
+						},
+						grid: {
+							row: {
+								colors: ['black', 'transparent'],
+								opacity: 0.3
+							}
+						}
+
+					}}
+				/>
+			)
+
+
+			}
+
+
 		</>
 	)
 }
