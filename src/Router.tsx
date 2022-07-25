22bc223f8336d59edd8e7562a0c3dad71072ba7f
YoungJinGo <skyyj32@naver.com>
2022-07-25 21:45:29 +0900

    Basename Setting

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coins from './routes/Coins';
 
 function Router() {
 	return (
-		<BrowserRouter basename='/chartCoin'>
+		<BrowserRouter basename='/cryptocurrencydata'>
 			<Switch>
 				<Route path="/:coinId">
 					<Coin />
